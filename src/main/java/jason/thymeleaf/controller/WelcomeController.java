239b759b20ff6a5daa82c84e257554fc0800d48e
keumhwanmoon <hwanth85@gmail.com>
2016-08-29 23:24:11 +0900

    Initial commit

---
+++ src/main/java/jason/thymeleaf/controller/WelcomeController.java
@@ -0,0 +1,18 @@
+package jason.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Jason on 2016-08-29.
+ */
+@Controller
+public class WelcomeController {
+    @GetMapping
+    public String index(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+}
